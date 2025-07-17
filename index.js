9c9504d05ca527d90751c9ee6f611bf0024cd496
saniya <saniyagarg9@gmail.com>
2025-07-17 13:37:19 +0530

    added the middlewares

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const users=require('./MOCK_DATA.json')
 const app=express()
 const PORT=8000
 app.use(express.urlencoded({extended:true}));
+app.use((req,res,next)=>
+{
+    console.log("hello from middleware1");
+    next();
+})
+app.use((req,res,next)=>
+{
+    fs.appendFile("log.txt",`\n${Date.now()}:${req.ip}${req.method}:${req.path}`,(err,data)=>
+    {
+        next();
+    })
+    console.log("hello from middleware 2");
+    next();
+})
 app.get("/users",(req,res)=>
 {
     const html=`
